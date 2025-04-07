ee307016246bb66af6d7ac2321e50dba6605542e
alvin081105 <ca081105@gmail.com>
2025-04-07 20:05:43 +0900

    Setting

---
+++ src/main/java/com/roomit/roomit/RoomitApplication.java
@@ -0,0 +1,13 @@
+package com.roomit.roomit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoomitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoomitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/roomit/roomit/RoomitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.roomit.roomit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoomitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
